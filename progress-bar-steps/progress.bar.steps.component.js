86094dd0a9e05d7d1b44114eeff10983dd82c819
david-ru <david.rubio@gmail.com>
2016-09-23 15:00:14 +0200

    Add files via upload
    Progress bar with hardcoded step description.

---
+++ progress-bar-steps/progress.bar.steps.component.js
@@ -0,0 +1,59 @@
+(function (angular) {
+  'use strict';
+
+  angular.module('progressBarSteps', [])
+    .component('progressBarSteps', {
+      templateUrl: 'components/commons/progress-bar-steps/progress-bar-steps.html',
+      controller: ProgressBarStepsController,
+      bindings: {
+        totalSteps: '<',
+        currentStep: '<',
+        sizeClass: '@'
+      }
+    })
+
+  function ProgressBarStepsController () {
+    var ctrl = this;
+
+    var ol = document.getElementById("progressBarSteps");
+    var estilos = "progress-bar-steps";
+    if (ctrl.sizeClass) {
+      estilos = estilos + " " + ctrl.sizeClass;
+    }
+    ol.setAttribute("class", estilos);
+    for (var i=1; i<=ctrl.totalSteps; i++) {
+      var li = document.createElement("li");
+      //li.setAttribute("data-step", i); //Información para dentro del paso
+      //li.appendChild(document.createTextNode(i)); //Información para debajo del paso
+
+
+// NOMBRES TEMPORALES !!!
+if (i===1) {
+  li.appendChild(document.createTextNode("1 Datos origen"));
+} else 
+if (i===2) {
+  li.appendChild(document.createTextNode("2 Datos destino"));
+} else {
+  li.appendChild(document.createTextNode("3 Resumen"));
+}
+// NOMBRES TEMPORALES !!!
+
+
+      if (i < ctrl.currentStep) {
+        li.setAttribute("class", "is-complete");
+      }
+      if (i === ctrl.currentStep) {
+        li.setAttribute("class", "is-active");
+      }
+      if (i === ctrl.totalSteps) {
+        li.setAttribute("class", "progress-bar-steps-last");
+      }
+      if (i === ctrl.currentStep && i === ctrl.totalSteps) {
+        li.setAttribute("class", "is-active progress-bar-steps-last");
+      }
+
+      ol.appendChild(li);
+    }
+  }
+    
+})(window.angular);
